8e9186f35c41c9b02278ded3815ff57b5b071ba7
juanitaNJ <juanitanarez@gmail.com>
2022-04-22 14:59:54 -0500

    Initial commit

---
+++ src/test/java/org/launchcode/hellospringliftoff/HelloSpringLiftoffApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.hellospringliftoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringLiftoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
